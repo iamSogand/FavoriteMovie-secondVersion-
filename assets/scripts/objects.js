3101080414db8c62497faaeba1ea721b7a7b9a07
imSogand <imsogand@outlook.com>
2025-01-14 15:43:11 +0100

    adding the filter functionality

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,7 +3,7 @@ const serachBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
   const movieList = document.getElementById('movie-list');
 
   if (movies.length === 0) {
@@ -13,7 +13,11 @@ const renderMovies = () => {
   }
   movieList.innerHTML = '';
 
-  movies.forEach((movie) => {
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
     const movieEl = document.createElement('li');
     let text = movie.info.title + '-';
     for (const key in movie.info) {
@@ -51,4 +55,10 @@ const addMovieHandler = () => {
   console.log(newMovie);
 };
 
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+};
+
 addMovieBtn.addEventListener('click', addMovieHandler);
+serachBtn.addEventListener('click', searchMovieHandler);
